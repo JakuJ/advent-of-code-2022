f148cafb58c5f46faf31957253b2395221dab04a
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2022-12-08 23:05:14 +0100

    Optimize part 2 of day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -61,52 +61,46 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let side = input.lines().next().unwrap().len();
-    let mut scores: Vec<(u32, u32, u32, u32)> = vec![(0, 0, 0, 0); side * side];
+    let mut scores = vec![(0, 0, 0, 0); side * side];
 
     let empty_seen = [0_usize; 10];
 
+    // top to bottom
+    let mut top_to_bottom = vec![empty_seen; side];
     for (y, line) in input.lines().enumerate() {
         // left to right
         let mut seen = empty_seen;
         for (x, c) in line.bytes().enumerate() {
             let h = (c - 48) as usize;
+            // left to right
             scores[x + y * side].0 = (x - seen[h]) as u32;
             for s in seen.iter_mut().take(h + 1) {
                 *s = x;
             }
-        }
-
-        // right to left
-        let mut seen = empty_seen;
-        for (x, c) in line.bytes().rev().enumerate() {
-            let h = (c - 48) as usize;
-            scores[(side - x - 1) + y * side].1 = (x - seen[h]) as u32;
-            for s in seen.iter_mut().take(h + 1) {
-                *s = x;
-            }
-        }
-    }
-
-    // top to bottom
-    let mut all_seen = vec![empty_seen; side];
-    for (y, line) in input.lines().enumerate() {
-        for (x, c) in line.bytes().enumerate() {
-            let h = (c - 48) as usize;
-            scores[x + y * side].2 = (y - all_seen[x][h]) as u32;
-            for s in all_seen[x].iter_mut().take(h + 1) {
+            // top to bottom
+            scores[x + y * side].1 = (y - top_to_bottom[x][h]) as u32;
+            for s in top_to_bottom[x].iter_mut().take(h + 1) {
                 *s = y;
             }
         }
     }
 
     // bottom to top
-    let mut all_seen = vec![empty_seen; side];
-    for (y, line) in input.lines().rev().enumerate() {
-        for (x, c) in line.bytes().enumerate() {
+    let mut bottom_to_top = vec![empty_seen; side];
+    for (ry, line) in input.lines().rev().enumerate() {
+        // right to left
+        let mut seen = empty_seen;
+        for (rx, c) in line.bytes().rev().enumerate() {
             let h = (c - 48) as usize;
-            scores[x + (side - y - 1) * side].3 = (y - all_seen[x][h]) as u32;
-            for s in all_seen[x].iter_mut().take(h + 1) {
-                *s = y;
+            // right to left
+            scores[(side - rx - 1) + (side - ry - 1) * side].2 = (rx - seen[h]) as u32;
+            for s in seen.iter_mut().take(h + 1) {
+                *s = rx;
+            }
+            // bottom to top
+            scores[(side - rx - 1) + (side - ry - 1) * side].3 = (ry - bottom_to_top[rx][h]) as u32;
+            for s in bottom_to_top[rx].iter_mut().take(h + 1) {
+                *s = ry;
             }
         }
     }
