385bf63241e97ad273ed5d2c24239994aee2f7be
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2022-12-13 00:50:57 +0100

    Refactor day 12

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,22 +1,23 @@
 #![feature(test)]
 
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashMap, VecDeque},
-};
-
 extern crate bucket_queue;
 use bucket_queue::*;
+use std::collections::HashMap;
 
 type Point = (i32, i32);
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord, Copy)]
+#[derive(Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 struct Node {
     coords: Point,
     value: char,
 }
 
-#[derive(Debug)]
+impl Node {
+    fn distance(&self, other: &Node) -> u32 {
+        ((self.coords.0 - other.coords.0).abs() + (self.coords.1 - other.coords.1).abs()) as u32
+    }
+}
+
 struct Graph {
     width: usize,
     vertices: Vec<Node>,
@@ -25,8 +26,10 @@ struct Graph {
     end: Node,
 }
 
-fn manhattan(a: Point, b: Point) -> u32 {
-    ((a.0 - b.0).abs() + (a.1 - b.1).abs()) as u32
+impl Graph {
+    pub fn index(&self, node: &Node) -> usize {
+        (node.coords.0 + node.coords.1 * self.width as i32) as usize
+    }
 }
 
 fn parse_graph_from_grid_of_adjacent_chars(input: &str, reverse: bool) -> Graph {
@@ -35,7 +38,7 @@ fn parse_graph_from_grid_of_adjacent_chars(input: &str, reverse: bool) -> Graph 
     let mut start = None;
     let mut end = None;
 
-    let width = input.lines().next().unwrap().len();
+    let width = input.lines().next().expect("Input cannot be empty").len();
 
     for (y, line) in input.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
@@ -63,7 +66,7 @@ fn parse_graph_from_grid_of_adjacent_chars(input: &str, reverse: bool) -> Graph 
     for node in &nodes {
         let mut adjacent_nodes = Vec::new();
         for other_node in &nodes {
-            if manhattan(node.coords, other_node.coords) == 1
+            if node.distance(other_node) == 1
                 && (if reverse {
                     node.value as i32 - other_node.value as i32 <= 1
                 } else {
@@ -80,21 +83,22 @@ fn parse_graph_from_grid_of_adjacent_chars(input: &str, reverse: bool) -> Graph 
         vertices: nodes,
         width,
         edges,
-        start: start.unwrap(),
-        end: end.unwrap(),
+        start: start.expect("No start node found"),
+        end: end.expect("No end node found"),
     }
 }
 
+/// A* algorithm for finding shortest path from Start node to End node.
 pub fn part_one(input: &str) -> Option<usize> {
     let graph = parse_graph_from_grid_of_adjacent_chars(input, false);
 
-    // define heuristic as integer euclidan distance to end
-    let heuristic = |node: Node| manhattan(node.coords, graph.end.coords);
+    let heuristic = |node: Node| node.distance(&graph.end) as usize;
 
     // find shortest path from start to end in graph using A* algorithm
     // https://en.wikipedia.org/wiki/A*_search_algorithm
-    let mut open_set = BinaryHeap::new();
-    open_set.push(Reverse((heuristic(graph.start), graph.start)));
+    let mut open_set = BucketQueue::<Vec<Node>>::new();
+
+    open_set.push(graph.start, heuristic(graph.start));
 
     let mut came_from = HashMap::new();
     let mut g_score = HashMap::new();
@@ -103,7 +107,7 @@ pub fn part_one(input: &str) -> Option<usize> {
     g_score.insert(graph.start, 0);
     f_score.insert(graph.start, heuristic(graph.start));
 
-    while let Some(Reverse((_, current))) = open_set.pop() {
+    while let Some(current) = open_set.pop_min() {
         if current == graph.end {
             let mut path = Vec::new();
             let mut current = current;
@@ -111,18 +115,16 @@ pub fn part_one(input: &str) -> Option<usize> {
                 path.push(current);
                 current = *prev;
             }
-            path.push(current);
-            path.reverse();
-            return Some(path.len() - 1);
+            return Some(path.len());
         }
 
-        for neighbor in graph.edges.get(&current).unwrap() {
-            let tentative_g_score = g_score[&current] + 1;
-            if tentative_g_score < *g_score.get(neighbor).unwrap_or(&u32::MAX) {
+        for neighbor in &graph.edges[&current] {
+            let tentative_g_score = g_score[&current] + 1_usize;
+            if tentative_g_score < *g_score.get(neighbor).unwrap_or(&usize::MAX) {
                 came_from.insert(neighbor, current);
                 g_score.insert(*neighbor, tentative_g_score);
                 f_score.insert(*neighbor, tentative_g_score + heuristic(*neighbor));
-                open_set.push(Reverse((f_score[neighbor], *neighbor)));
+                open_set.push(*neighbor, f_score[neighbor]);
             }
         }
     }
@@ -130,43 +132,35 @@ pub fn part_one(input: &str) -> Option<usize> {
     None
 }
 
+/// Dial's algorithm for finding paths from the End node to all other nodes.
+/// Complexity: O(E + V), but since E = 4V, this is O(V)
 pub fn part_two(input: &str) -> Option<usize> {
     let graph = parse_graph_from_grid_of_adjacent_chars(input, true);
 
-    // Dial's algorithm for finding paths from end to all other nodes
-    let mut dist = vec![usize::MAX; graph.edges.len()];
-    let mut queue = BucketQueue::<VecDeque<Node>>::new();
+    let mut dist = vec![usize::MAX; graph.vertices.len()];
+    let mut queue = BucketQueue::<Vec<Node>>::new();
 
-    let index = |node: &Node| (node.coords.0 + node.coords.1 * graph.width as i32) as usize;
+    dist[graph.index(&graph.end)] = 0;
+    queue.push(graph.end, 0);
 
-    dist[index(&graph.end)] = 0;
-    queue.enqueue(graph.end, 0);
+    while let Some(current) = queue.pop_min() {
+        let dist_c_p1 = dist[graph.index(&current)] + 1;
 
-    while let Some(current) = queue.dequeue_min() {
-        let c_ix = index(&current);
+        for neighbor in &graph.edges[&current] {
+            let n_ix = graph.index(neighbor);
 
-        for neighbor in graph.edges.get(&current).unwrap() {
-            let n_ix = index(neighbor);
-
-            let dist_c = dist[c_ix];
-            let dist_n = dist[n_ix];
-
-            if dist_n > dist_c + 1 {
-                dist[n_ix] = dist_c + 1;
-                queue.enqueue(*neighbor, dist_c + 1);
+            if dist[n_ix] > dist_c_p1 {
+                dist[n_ix] = dist_c_p1;
+                queue.push(*neighbor, dist_c_p1);
             }
         }
     }
 
-    Some(
-        *dist
-            .iter()
-            .enumerate()
-            .filter(|&(ix, _)| graph.vertices[ix].value == 'a')
-            .map(|(_, b)| b)
-            .min()
-            .unwrap_or(&0),
-    )
+    dist.iter()
+        .enumerate()
+        .filter(|&(ix, _)| graph.vertices[ix].value == 'a')
+        .map(|(_, &d)| d)
+        .min()
 }
 
 fn main() {
